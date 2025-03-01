e2004c2d4ec6694a856c1e6b514fa03dec5f0c0c
Ariel Patiño <ariel.patino@gmail.com>
2025-03-01 12:58:34 -0500

    feat: Add Product, Category & Order Models
     Changes to be committed:
    	new file:   app/Models/Category.php
    	new file:   app/Models/Order.php
    	new file:   app/Models/Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'category_id',
+        'image'
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
